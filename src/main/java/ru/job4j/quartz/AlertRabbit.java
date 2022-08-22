227cffe52136c559ee7fedeee1070d644c762a4e
Exsnipe <exsnipe@yandex.ru>
2022-08-22 03:56:11 -0700

    Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -8,41 +8,72 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 public class AlertRabbit {
+    private static Properties cfg;
+    private static Connection cn;
+
     public static void main(String[] args) {
+        init();
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("connection", cn);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(getTimePeriod("rabbit.properties"))
+                    .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
+            Thread.sleep(10000);
+            scheduler.shutdown();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public static int getTimePeriod(String filePath) {
-        Properties cfg = new Properties();
-        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream(filePath)) {
+    public static void init() {
+        cfg = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
             cfg.load(in);
         } catch (NullPointerException | IOException ex) {
             throw new IllegalArgumentException();
         }
-        return Integer.parseInt(cfg.getProperty("rabbit.interval"));
+        try {
+            Class.forName(cfg.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(cfg.getProperty("url"),
+                    cfg.getProperty("username"), cfg.getProperty("password"));
+            try (Statement st = cn.createStatement()) {
+                st.execute("truncate rabbit");
+            } catch (SQLException sqlEx) {
+                sqlEx.printStackTrace();
+            }
+        } catch (ClassNotFoundException | SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Connection cnFromContext = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            try (PreparedStatement ps = cnFromContext
+                   .prepareStatement("insert into rabbit (created_date) values (?)")) {
+                ps.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                ps.execute();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+            }
         }
     }
 }
