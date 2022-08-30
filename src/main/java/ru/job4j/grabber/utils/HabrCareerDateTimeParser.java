06a819ba958480326116e1225140c0cc6c506404
Exsnipe <exsnipe@yandex.ru>
2022-08-30 20:03:06 +0300

    Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -3,6 +3,7 @@ package ru.job4j.grabber.utils;
 import ru.job4j.grabber.HabrCareerParse;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
     @Override
@@ -10,11 +11,7 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
         if (!parse.matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\+\\d{2}:\\d{2}")) {
             throw new IllegalArgumentException();
         }
-        String[] dateTimeArray = parse.split("T");
-        String[] dateArray = dateTimeArray[0].split("-");
-        String[] timeArray = dateTimeArray[1].split("[:\\+]");
-        return LocalDateTime.of(Integer.parseInt(dateArray[0]), Integer.parseInt(dateArray[1]),
-                Integer.parseInt(dateArray[2]), Integer.parseInt(timeArray[0]),
-                Integer.parseInt(timeArray[1]), Integer.parseInt(timeArray[2]));
+        ZonedDateTime zdt = ZonedDateTime.parse(parse);
+        return zdt.toLocalDateTime();
     }
 }
