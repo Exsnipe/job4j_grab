643518cb3a72c66c9907ed6536a8e8717c381c4b
Exsnipe <exsnipe@yandex.ru>
2022-08-30 20:10:22 +0300

    Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -12,19 +12,21 @@ public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String
-            .format("%s/vacancies/java_developer", SOURCE_LINK);
+            .format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-       Connection connection = Jsoup.connect(PAGE_LINK);
-       Document document = connection.get();
-       Elements rows = document.select(".vacancy-card__inner");
-       rows.forEach(row -> {
-           Element titleElement = row.select(".vacancy-card__title").first();
-           Element linkElement = titleElement.child(0);
-           Element dateTime = row.select(".vacancy-card__date").first();
-           String date = dateTime.child(0).attr("datetime");
-           String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-           System.out.printf("%s %s %s%n", titleElement.text(), link, date);
-       });
+       for (int i = 1; i <= 5; i++) {
+           Connection connection = Jsoup.connect(String.format("%s%d", PAGE_LINK, i));
+           Document document = connection.get();
+           Elements rows = document.select(".vacancy-card__inner");
+           rows.forEach(row -> {
+               Element titleElement = row.select(".vacancy-card__title").first();
+               Element linkElement = titleElement.child(0);
+               Element dateTime = row.select(".vacancy-card__date").first();
+               String date = dateTime.child(0).attr("datetime");
+               String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+               System.out.printf("%s %s %s%n", titleElement.text(), link, date);
+           });
+       }
     }
 }
